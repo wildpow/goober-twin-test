027a1dc655461cbdce0a74cf2d8ac9a043659d07
wildpow <element1028@gmail.com>
2021-04-08 16:50:50 -0700

    test

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,11 @@
 import React from "react"
+import tw,{ styled } from 'twin.macro';
 
+const Button = styled('button')`
+/* background-color: green; */
+font-size: 10em;
+${tw`bg-green-900`};
+`
 export default function Home() {
-  return <div>Hello world!</div>
+  return <div><Button>Doesn't work</Button>Hello world!</div>
 }
